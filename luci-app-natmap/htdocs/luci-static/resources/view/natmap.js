4057c2bf08cd83eaea9ead31be72c7d517bf4bd4
guguji <guguji@local>
2023-10-01 19:31:24 +0800

    update

--- luci-app-natmap/htdocs/luci-static/resources/view/natmap.js
+++ luci-app-natmap/htdocs/luci-static/resources/view/natmap.js
@@ -13,38 +13,38 @@ var callServiceList = rpc.declare({
 });
 
 function getInstances() {
-	return L.resolveDefault(callServiceList('natmap'), {}).then(function(res) {
+	return L.resolveDefault(callServiceList('natmap'), {}).then(function (res) {
 		try {
 			return res.natmap.instances || {};
-		} catch (e) {}
+		} catch (e) { }
 		return {};
 	});
 }
 
 function getStatus() {
-	return getInstances().then(function(instances) {
+	return getInstances().then(function (instances) {
 		var promises = [];
 		var status = {};
 		for (var key in instances) {
 			var i = instances[key];
 			if (i.running && i.pid) {
 				var f = '/var/run/natmap/' + i.pid + '.json';
-				(function(k) {
-					promises.push(fs.read(f).then(function(res) {
+				(function (k) {
+					promises.push(fs.read(f).then(function (res) {
 						status[k] = JSON.parse(res);
-					}).catch(function(e){}));
+					}).catch(function (e) { }));
 				})(key);
 			}
 		}
-		return Promise.all(promises).then(function() { return status; });
+		return Promise.all(promises).then(function () { return status; });
 	});
 }
 
 return view.extend({
-	load: function() {
+	load: function () {
 		return getStatus();
 	},
-	render: function(status) {
+	render: function (status) {
 		var m, s, o;
 
 		m = new form.Map('natmap', _('NATMap'));
@@ -53,12 +53,13 @@ return view.extend({
 		s.anonymous = true;
 
 		s.tab('general', _('General Settings'));
+		s.tab('forward', _('Forward Settings'));
 		s.tab('notify', _('Notify Settings'));
 		s.tab('link', _('Link Settings'));
-		
+
 		o = s.option(form.DummyValue, '_nat_name', _('Name'));
 		o.modalonly = false;
-		o.textvalue = function(section_id) {
+		o.textvalue = function (section_id) {
 			var s = status[section_id];
 			if (s) return s.name;
 		};
@@ -67,28 +68,25 @@ return view.extend({
 		o.datatype = 'string';
 		o.modalonly = true;
 
-		o = s.taboption('general', form.ListValue, 'udp_mode', _('Protocol'));
-		o.default = '1';
+		o = s.taboption('general', form.ListValue, 'nat_protocol', _('Protocol'));
+		o.default = '0';
 		o.value('0', 'TCP');
 		o.value('1', 'UDP');
-		o.textvalue = function(section_id) {
+		o.textvalue = function (section_id) {
 			var cval = this.cfgvalue(section_id);
 			var i = this.keylist.indexOf(cval);
 			return this.vallist[i];
 		};
 
-		o = s.taboption('general', form.ListValue, 'family', _('Restrict to address family'));
+		o = s.taboption('general', form.ListValue, 'ip_address_family', _('Restrict to address family'));
 		o.modalonly = true;
 		o.value('', _('IPv4 and IPv6'));
 		o.value('ipv4', _('IPv4 only'));
 		o.value('ipv6', _('IPv6 only'));
 
-		o = s.taboption('general', widgets.NetworkSelect, 'source_interface', _('Source_Interface'));
+		o = s.taboption('general', widgets.NetworkSelect, 'wan_interface', _('Wan_Interface'));
 		o.modalonly = true;
 
-		o = s.taboption('general', widgets.NetworkSelect, 'target_interface', _('Target_Interface'));
-		o.modalonly = true;
-		
 		o = s.taboption('general', form.Value, 'interval', _('Keep-alive interval'));
 		o.datatype = 'uinteger';
 		o.modalonly = true;
@@ -104,72 +102,117 @@ return view.extend({
 		o.modalonly = true;
 		o.rmempty = false;
 
-		o = s.taboption('general', form.Value, 'port', _('Bind port'));
+		o = s.taboption('general', form.Value, 'bind_port', _('Bind port'));
 		o.datatype = 'port';
 		o.rmempty = false;
-
-		o = s.taboption('general', form.Flag, '_forward_mode', _('Forward mode'));
-		o.modalonly = true;
-		o.ucioption = 'forward_target';
-		o.load = function(section_id) {
-			return this.super('load', section_id) ? '1' : '0';
-		};
-		o.write = function(section_id, formvalue) {};
-
-		o = s.taboption('general', form.Value, 'forward_target', _('Forward target'));
-		o.datatype = 'host';
-		o.modalonly = true;
-		o.depends('_forward_mode', '1');
-
-		o = s.taboption('general', form.Value, 'forward_port', _('Forward target port'), _('0 will forward to the out port get from STUN'));
-		o.datatype = 'port';
-		o.modalonly = true;
-		o.depends('_forward_mode', '1');
-
-		o = s.taboption('general', form.Flag, 'forward_use_natmap', _('Forward use natmap'));
-		o.editable = true;
-		o.default = false;
-		o.modalonly = true;
 
 		o = s.taboption('general', form.Value, 'notify_script', _('Notify script'));
 		o.datatype = 'file';
 		o.modalonly = true;
 
-		o = s.taboption('notify', form.Flag, 'im_notify_enable', _('Notify'));
+		//
+		// 
+		// forward
+		o = s.taboption('forward', form.Flag, 'forward_enable', _('Forward Enable'));
 		o.default = false;
 		o.modalonly = true;
 
-		o = s.taboption('notify', form.ListValue, 'im_notify_channel', _('Notify channel'));
+		o = s.taboption('forward', form.ListValue, 'forward_mode', _('Forward mode'));
+		o.default = 'local';
+		o.value('local', _('local'));
+		o.value('ikuai', _('ikuai'));
+		o.depends('forward_enable', '1');
+
+		// forward_natmap
+		o = s.taboption('forward', form.Value, 'forward_target', _('Forward target'));
+		o.datatype = 'host';
+		o.modalonly = true;
+		o.depends('forward_mode', 'local');
+		o.depends('forward_mode', 'ikuai');
+
+		o = s.taboption('forward', form.Value, 'forward_port', _('Forward target port'), _('0 will forward to the out port get from STUN'));
+		o.datatype = 'port';
+		o.modalonly = true;
+		o.depends('forward_mode', 'local');
+		o.depends('forward_mode', 'ikuai');
+
+		o = s.taboption('forward', widgets.NetworkSelect, 'target_interface', _('Target_Interface'));
+		o.modalonly = true;
+		o.depends('forward_mode', 'local');
+
+		o = s.taboption('forward', form.Flag, 'forward_use_natmap', _('Forward use natmap'));
+		o.default = false;
+		o.modalonly = true;
+		o.depends('forward_mode', 'local');
+
+		// forward_ikuai
+		o = s.taboption('forward', form.Value, 'ikuai_web_url', _('Ikuai Web URL'), _('such as http://127.0.0.1:8080'));
+		o.datatype = 'string';
+		o.modalonly = true;
+		o.depends('forward_mode', 'ikuai');
+
+		o = s.taboption('forward', form.Value, 'ikuai_username', _('Ikuai Username'));
+		o.datatype = 'string';
+		o.modalonly = true;
+		o.depends('forward_mode', 'ikuai');
+
+		o = s.taboption('forward', form.Value, 'ikuai_password', _('Ikuai Password'));
+		o.datatype = 'string';
+		o.modalonly = true;
+		o.depends('forward_mode', 'ikuai');
+
+		o = s.taboption('forward', form.ListValue, 'ikuai_mapping_protocol', _('Ikuai Mapping Protocol'));
+		o.modalonly = true;
+		o.value('tcp+udp', _('TCP+UDP'));
+		o.value('tcp', _('TCP'));
+		o.value('udp', _('UDP'));
+		o.depends('forward_mode', 'ikuai');
+
+		o = s.taboption('forward', form.Value, 'ikuai_mapping_wan_interface', _('Ikuai Mapping Wan Interface'));
+		o.datatype = 'string';
+		o.modalonly = true;
+		o.depends('forward_mode', 'ikuai');
+
+		//
+		// 
+		// notify
+		o = s.taboption('notify', form.Flag, 'notify_enable', _('Notify'));
+		o.default = false;
+		o.modalonly = true;
+
+		o = s.taboption('notify', form.ListValue, 'notify_channel', _('Notify channel'));
 		o.default = 'telegram_bot';
 		o.modalonly = true;
 		o.value('telegram_bot', _('Telegram Bot'));
 		o.value('pushplus', _('PushPlus'));
-		o.depends('im_notify_enable', '1');
+		o.depends('notify_enable', '1');
 
-		o = s.taboption('notify', form.Value, 'im_notify_channel_telegram_bot_chat_id', _('Chat ID'));
+		o = s.taboption('notify', form.Value, 'notify_channel_telegram_bot_chat_id', _('Chat ID'));
 		o.datatype = 'string';
 		o.modalonly = true;
-		o.depends('im_notify_channel', 'telegram_bot');
+		o.depends('notify_channel', 'telegram_bot');
 
-		o = s.taboption('notify', form.Value, 'im_notify_channel_telegram_bot_token', _('Token'));
+		o = s.taboption('notify', form.Value, 'notify_channel_telegram_bot_token', _('Token'));
 		o.datatype = 'string';
 		o.modalonly = true;
-		o.depends('im_notify_channel', 'telegram_bot');
+		o.depends('notify_channel', 'telegram_bot');
 
-		o = s.taboption('notify', form.Value, 'im_notify_channel_pushplus_token', _('Token'));
+		o = s.taboption('notify', form.Value, 'notify_channel_pushplus_token', _('Token'));
 		o.datatype = 'string';
 		o.modalonly = true;
-		o.depends('im_notify_channel', 'pushplus');
+		o.depends('notify_channel', 'pushplus');
 
+
+		// link
 		o = s.taboption('link', form.Flag, '_link_to', _('Change another service\'s config'));
 		o.modalonly = true;
-		o.ucioption = 'mode';
-		o.load = function(section_id) {
+		o.ucioption = 'link_mode';
+		o.load = function (section_id) {
 			return this.super('load', section_id) ? '1' : '0';
 		};
-		o.write = function(section_id, formvalue) {};
+		o.write = function (section_id, formvalue) { };
 
-		o = s.taboption('link', form.ListValue, 'mode', _('Service'));
+		o = s.taboption('link', form.ListValue, 'link_mode', _('Service'));
 		o.default = 'qbittorrent';
 		o.modalonly = true;
 		o.value('emby', _('Emby'));
@@ -182,71 +225,71 @@ return view.extend({
 		o = s.taboption('link', form.Value, 'cloudflare_email', _('Email'));
 		o.datatype = 'string';
 		o.modalonly = true;
-		o.depends('mode', 'cloudflare_origin_rule');
-		o.depends('mode', 'cloudflare_redirect_rule');
+		o.depends('link_mode', 'cloudflare_origin_rule');
+		o.depends('link_mode', 'cloudflare_redirect_rule');
 
 		o = s.taboption('link', form.Value, 'cloudflare_api_key', _('API Key'));
 		o.datatype = 'string';
 		o.modalonly = true;
-		o.depends('mode', 'cloudflare_origin_rule');
-		o.depends('mode', 'cloudflare_redirect_rule');
+		o.depends('link_mode', 'cloudflare_origin_rule');
+		o.depends('link_mode', 'cloudflare_redirect_rule');
 
 		o = s.taboption('link', form.Value, 'cloudflare_zone_id', _('Zone ID'));
 		o.datatype = 'string';
 		o.modalonly = true;
-		o.depends('mode', 'cloudflare_origin_rule');
-		o.depends('mode', 'cloudflare_redirect_rule');
+		o.depends('link_mode', 'cloudflare_origin_rule');
+		o.depends('link_mode', 'cloudflare_redirect_rule');
 
 		o = s.taboption('link', form.Value, 'cloudflare_rule_name', _('Rule Name'));
 		o.datatype = 'string';
 		o.modalonly = true;
-		o.depends('mode', 'cloudflare_origin_rule');
-		o.depends('mode', 'cloudflare_redirect_rule');
-		
+		o.depends('link_mode', 'cloudflare_origin_rule');
+		o.depends('link_mode', 'cloudflare_redirect_rule');
+
 		o = s.taboption('link', form.Value, 'cloudflare_rule_target_url', _('Target URL'));
 		o.datatype = 'string';
 		o.modalonly = true;
-		o.depends('mode', 'cloudflare_redirect_rule');
+		o.depends('link_mode', 'cloudflare_redirect_rule');
 
 		o = s.taboption('link', form.Value, 'emby_url', _('URL'));
 		o.datatype = 'string';
 		o.modalonly = true;
-		o.depends('mode', 'emby');
+		o.depends('link_mode', 'emby');
 
 		o = s.taboption('link', form.Value, 'emby_api_key', _('API Key'));
 		o.datatype = 'host';
 		o.modalonly = true;
-		o.depends('mode', 'emby');
+		o.depends('link_mode', 'emby');
 
 		o = s.taboption('link', form.Flag, 'emby_use_https', _('Update HTTPS Port'), _('Set to False if you want to use HTTP'));
 		o.default = false;
 		o.modalonly = true;
-		o.depends('mode', 'emby');
+		o.depends('link_mode', 'emby');
 
 		o = s.taboption('link', form.Flag, 'emby_update_host_with_ip', _('Update host with IP'));
 		o.default = false;
 		o.modalonly = true;
-		o.depends('mode', 'emby');
+		o.depends('link_mode', 'emby');
 
 		o = s.taboption('link', form.Value, 'qb_web_ui_url', _('Web UI URL'));
 		o.datatype = 'string';
 		o.modalonly = true;
-		o.depends('mode', 'qbittorrent');
-		
+		o.depends('link_mode', 'qbittorrent');
+
 		o = s.taboption('link', form.Value, 'qb_username', _('Username'));
 		o.datatype = 'string';
 		o.modalonly = true;
-		o.depends('mode', 'qbittorrent');
+		o.depends('link_mode', 'qbittorrent');
 
 		o = s.taboption('link', form.Value, 'qb_password', _('Password'));
 		o.datatype = 'string';
 		o.modalonly = true;
-		o.depends('mode', 'qbittorrent');
+		o.depends('link_mode', 'qbittorrent');
 
 		o = s.taboption('link', form.Flag, 'qb_allow_ipv6', _('Allow IPv6'));
 		o.default = false;
 		o.modalonly = true;
-		o.depends('mode', 'qbittorrent');
+		o.depends('link_mode', 'qbittorrent');
 
 		o = s.taboption('link', form.Value, 'qb_ipv6_address', _('IPv6 Address'));
 		o.datatype = 'string';
@@ -256,22 +299,22 @@ return view.extend({
 		o = s.taboption('link', form.Value, 'tr_rpc_url', _('RPC URL'));
 		o.datatype = 'string';
 		o.modalonly = true;
-		o.depends('mode', 'transmission');
-		
+		o.depends('link_mode', 'transmission');
+
 		o = s.taboption('link', form.Value, 'tr_username', _('Username'));
 		o.datatype = 'string';
 		o.modalonly = true;
-		o.depends('mode', 'transmission');
+		o.depends('link_mode', 'transmission');
 
 		o = s.taboption('link', form.Value, 'tr_password', _('Password'));
 		o.datatype = 'string';
 		o.modalonly = true;
-		o.depends('mode', 'transmission');
+		o.depends('link_mode', 'transmission');
 
 		o = s.taboption('link', form.Flag, 'tr_allow_ipv6', _('Allow IPv6'));
 		o.modalonly = true;
 		o.default = false;
-		o.depends('mode', 'transmission');
+		o.depends('link_mode', 'transmission');
 
 		o = s.taboption('link', form.Value, 'tr_ipv6_address', _('IPv6 Address'));
 		o.datatype = 'string';
@@ -280,14 +323,14 @@ return view.extend({
 
 		o = s.option(form.DummyValue, '_external_ip', _('External IP'));
 		o.modalonly = false;
-		o.textvalue = function(section_id) {
+		o.textvalue = function (section_id) {
 			var s = status[section_id];
 			if (s) return s.ip;
 		};
 
 		o = s.option(form.DummyValue, '_external_port', _('External Port'));
 		o.modalonly = false;
-		o.textvalue = function(section_id) {
+		o.textvalue = function (section_id) {
 			var s = status[section_id];
 			if (s) return s.port;
 		};
---
+++ natmap/files/link.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# NATMap
+outter_ip=$1
+outter_port=$2
+ip4p=$3
+inner_port=$4
+protocol=$5
+
+INTERNAL_DEFINE_SCRIPT=""
+case $LINK_MODE in
+"qbittorrent")
+    INTERNAL_DEFINE_SCRIPT="/usr/lib/natmap/plugin/qb.sh"
+    ;;
+"transmission")
+    INTERNAL_DEFINE_SCRIPT="/usr/lib/natmap/plugin/tr.sh"
+    ;;
+"emby")
+    INTERNAL_DEFINE_SCRIPT="/usr/lib/natmap/plugin/emby.sh"
+    ;;
+"cloudflare_origin_rule")
+    INTERNAL_DEFINE_SCRIPT="/usr/lib/natmap/plugin/cloudflare_origin_rule.sh"
+    ;;
+"cloudflare_redirect_rule")
+    INTERNAL_DEFINE_SCRIPT="/usr/lib/natmap/plugin/cloudflare_redirect_rule.sh"
+    ;;
+*)
+    INTERNAL_DEFINE_SCRIPT=""
+    ;;
+esac
+
+if [ ! -z "$INTERNAL_DEFINE_SCRIPT" ]; then
+    echo "$NAT_NAME Execute internal define script: $INTERNAL_DEFINE_SCRIPT"
+    source $INTERNAL_DEFINE_SCRIPT "$@"
+fi
